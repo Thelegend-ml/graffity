33f5e8692d2bf3b970ffd9b3c65f9dd3da548ec9
Manuel Lopez <122247515+mlopezmaya@users.noreply.github.com>
2023-05-20 01:06:51 -0700

    Version 1.1

--- script.js
+++ script.js
@@ -183,6 +183,28 @@ projects.forEach(project => {
 // End of Prtojects 
 
 // Section 5
+// Form 
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+    input.addEventListener("focus", () => {
+        formHeading.style.opacity = "0" 
+        setTimeout(() => {
+            formHeading.textContent = `Your ${input.placeholder}`;
+            formHeading.style.opacity = "1";
+        }, 300);
+    });
+
+    input.addEventListener("blur", () => {
+        formHeading.style.opacity = "0";
+        setTimeout(() => {
+            formHeading.textContent = "Let's Talk";
+            formHeading.style.opacity = "1";
+        }, 300);
+        });
+    }); 
+// End of Form
 // Form Validation
 const form = document.querySelector(".contact-form");
 const username = document.getElementById("name");
